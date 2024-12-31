c370771510e52605f83e5b120004a08e9ddfc944
shamitha <2023it0823@svce.ac.in>
2024-12-31 16:18:40 +0530

    contact page edited

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <div className="home-container">
       <div className="welcome-text">
-        <h1>Contact</h1>
+        <h1>Contact edited</h1>
         <p>Your one-stop solution for everything you need edited.</p>
       </div>
       <Footer />
