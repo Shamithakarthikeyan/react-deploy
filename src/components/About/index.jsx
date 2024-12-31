8928f4064495c86d792570c6f73c9dd3a2aec2db
shamitha <2023it0823@svce.ac.in>
2024-12-31 16:15:46 +0530

    video addedd in about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -6,8 +6,9 @@ const About = () => {
   return (
     <div className="home-container">
       <div className="welcome-text">
-        <h1>About</h1>
+        <h1>About edited</h1>
         <p>Your one-stop solution for everything you need.</p>
+        <iframe width="1000" height="500" src="https://www.youtube.com/embed/GV3HUDMQ-F8?si=K85c4Ug9J67zPIRj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
       <Footer />
 
